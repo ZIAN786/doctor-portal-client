d89b080abc61403b8f59ea87374698d63dfd8af6
ZIAN786 <zakirzian41@gmail.com>
2022-08-15 01:51:01 +0600

    client part

---
+++ src/components/Login/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBIvVzoQ7_eNqvtr9Kapuh-eQuAEwNbkYc",
+  authDomain: "doctors-portal-90b2a.firebaseapp.com",
+  databaseURL: "https://doctors-portal-90b2a.firebaseio.com",
+  projectId: "doctors-portal-90b2a",
+  storageBucket: "doctors-portal-90b2a.appspot.com",
+  messagingSenderId: "456385650067",
+  appId: "1:456385650067:web:2edffa58815dde0f081c66",
+};
+export default firebaseConfig;
